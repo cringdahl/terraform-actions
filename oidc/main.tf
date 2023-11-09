8aaa98db93a7fda9a661fe131fc0bbba9211aa2b
Cory Ringdahl <cory.ringdahl@rackspace.com>
2023-11-09 16:18:11 -0600

    update assumerole, fmt

--- oidc/main.tf
+++ oidc/main.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_policy" "s3" {
-  description = "Minimal required permissions to use Terraform state files"
+  description = "Minimal required permissions to use Terraform state files and OIDC policy"
   name        = "s3TerraformState"
   policy = jsonencode(
     {
@@ -10,6 +10,9 @@ resource "aws_iam_policy" "s3" {
             "s3:GetObject",
             "s3:ListBucket",
             "s3:DeleteObject",
+            "iam:GetRole",
+            "iam:GetPolicy",
+            "iam:GetOpenIDConnectProvider",
           ]
           Effect   = "Allow"
           Resource = "*"
@@ -41,7 +44,7 @@ data "aws_iam_policy_document" "oidc" {
     condition {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
-      values   = ["repo:RSS-Engineering/undercloud:*", "repo:cringdahl/terraform-actions"]
+      values   = ["repo:RSS-Engineering/undercloud:*", "repo:cringdahl/terraform-actions:*"]
     }
 
     principals {
@@ -52,14 +55,14 @@ data "aws_iam_policy_document" "oidc" {
 }
 
 resource "aws_iam_role" "oidc" {
-  name                 = "githubActionsAccessS3TerraformState"
-  assume_role_policy   = data.aws_iam_policy_document.oidc.json
-  description          = "For use with OIDC Github integration"
+  name               = "githubActionsAccessS3TerraformState"
+  assume_role_policy = data.aws_iam_policy_document.oidc.json
+  description        = "For use with OIDC Github integration"
   managed_policy_arns  = [aws_iam_policy.s3.arn]
   max_session_duration = 3600
 }
 
-output oidc_arn {
+output "oidc_arn" {
   value       = aws_iam_role.oidc.arn
   description = "Role ARN for use with AWS Credentials Action"
 }
